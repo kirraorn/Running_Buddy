787ca6fa0aa29e9a376619662f69f59e1b833861
RileyGalpin <rg22bf@fsu.edu>
2026-02-18 20:50:01 -0500

    Created Goals class

---
+++ RunningBuddy/Models/Goals.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunningBuddy.Models
+{
+    internal class Goals
+    {
+        public int Id { get; set; }
+        public double Distance { get; set; }
+        public DateTime Time { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
--- RunningBuddy/obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/RunningBuddy.AssemblyInfo.cs
+++ RunningBuddy/obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/RunningBuddy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RunningBuddy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6ff0649d13780664dc30b651afcdc4a488d1bf41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+12d64573b747dd80c82d3b301a3ebf224d948eef")]
 [assembly: System.Reflection.AssemblyProductAttribute("RunningBuddy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RunningBuddy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
