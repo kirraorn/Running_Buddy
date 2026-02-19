52b33066b460ebadb8fc6ba8f1f6dfc246d1b84a
lchang48 <laurenchang26@gmail.com>
2026-02-19 10:57:16 -0500

    testing added weather class; closes #14

---
+++ RunningBuddy/Models/Weather.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunningBuddy.Models
+{
+    internal class Weather
+    {
+        public double Humidity { get; set;}
+        public double currentTemp { get; set;}
+        public double windSpeed {get; set;}
+        public string harshConditions {get; set;}
+    }
+}
