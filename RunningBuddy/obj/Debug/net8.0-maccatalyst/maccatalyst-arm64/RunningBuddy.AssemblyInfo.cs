2fdbb7aa0f46e895a2d0e3fea63e9af2f8558298
RileyGalpin <rg22bf@fsu.edu>
2026-02-20 06:14:57 -0500

    Created goals class, closes #21

--- RunningBuddy/obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/RunningBuddy.AssemblyInfo.cs
+++ RunningBuddy/obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/RunningBuddy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RunningBuddy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6ff0649d13780664dc30b651afcdc4a488d1bf41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+9abb57626dc74fc3bca2f4b8d05e54813b83a87f")]
 [assembly: System.Reflection.AssemblyProductAttribute("RunningBuddy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RunningBuddy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
