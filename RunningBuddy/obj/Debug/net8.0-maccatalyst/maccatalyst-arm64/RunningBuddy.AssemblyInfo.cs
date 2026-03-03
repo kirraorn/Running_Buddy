7c8d992d7dd88e2f12d20fb7fe67565c65f3b11b
RileyGalpin <rg22bf@fsu.edu>
2026-03-02 19:16:10 -0500

    fixed wording, closes #67

--- RunningBuddy/obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/RunningBuddy.AssemblyInfo.cs
+++ RunningBuddy/obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/RunningBuddy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RunningBuddy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5bbe296a54e416e3f5657b8624ef99c9f150de7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ac372da055d330b9d5e156785e7747d2cb9d5ad0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RunningBuddy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RunningBuddy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
