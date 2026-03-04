e2ee225bca2bfd6010cf00a5e64130a4583e0a89
kirraorn <kno22a@fsu.edu>
2026-03-03 20:58:24 -0500

    feat: updated MainPage UI and fixed startup connectivity logic; closes #75

--- RunningBuddy/Views/MainPage.xaml.cs
+++ RunningBuddy/Views/MainPage.xaml.cs
@@ -1,41 +1,52 @@
 ﻿using Microsoft.Maui.Networking;
+using System.Net.NetworkInformation; // Added this for NetworkInterface
 
 namespace RunningBuddy.Views;
 
 public partial class MainPage : ContentPage
 {
-	Models.User user = new Models.User(); //currently creating an instance of the user class, to check connectivity, this should probably be refactored
-	Models.Weather currentWeather;
-	int count = 0;
-	
-	
-	public MainPage()
-	{
-		InitializeComponent();
-		
-        if (!isNetworkConnected()) //if offline go to manual weather input
-			{
-				Shell.Current.GoToAsync("//ManualWeatherInputView");
-			}
-		
+    // Initialize user and weather
+    Models.User user = new Models.User(); 
+    // Made this nullable to fix the warning CS8618
+    Models.Weather? currentWeather;
 
-	}
-
-	private void ProfileClicked(object sender, EventArgs e)
-	{
-        Shell.Current.GoToAsync("//UserProfile");
-    }
-
-	private bool isNetworkConnected() //need to call every time page is refreshed
+    public MainPage()
     {
-		IEnumerable<ConnectionProfile> profiles = Connectivity.Current.ConnectionProfiles;
-		if (profiles.Contains(ConnectionProfile.WiFi))
-			{
-				return true;
-			}
-			return false;
-
+        InitializeComponent();
+        CheckConnectivity();
     }
 
-}
+    private void CheckConnectivity()
+    {
+        // Using MAUI's built-in Connectivity API
+        if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
+        {
+            // If no internet, go to manual input
+            Shell.Current.GoToAsync("//ManualWeatherInputView");
+        }
+    }  
 
+    // This helper method check if WiFi is specifically being used
+    private bool IsWifiConnected()
+    {
+        var profiles = Connectivity.Current.ConnectionProfiles;
+        return profiles.Contains(ConnectionProfile.WiFi);
+    }
+
+    private bool IsNetworkConnected() 
+    {
+       return NetworkInterface.GetIsNetworkAvailable();
+    }
+
+    // Navigation methods
+    private async void ProfileClicked(object sender, EventArgs e) => 
+        await Shell.Current.GoToAsync("//UserProfile");
+    private async void HomeClicked(object sender, EventArgs e) => 
+        await Shell.Current.GoToAsync("//MainPage");
+    private async void CalendarClicked(object sender, EventArgs e) => 
+        await Shell.Current.GoToAsync("//CalendarPage");
+    private async void ShoesClicked(object sender, EventArgs e) => 
+        await Shell.Current.GoToAsync("//ShoesPage");
+    private async void RoutesClicked(object sender, EventArgs e) => 
+        await Shell.Current.GoToAsync("//RoutesPage");
+}
